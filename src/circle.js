2ca9ea64370f1e0b74c9bd2a92e6c566d91d7e1c
Suzi Rubino <suzi@suzirubi.com>
2020-01-30 19:18:23 -0800

    finished business and user interface logic for circle area. Works as expected

--- src/circle.js
+++ src/circle.js
@@ -1,7 +1,6 @@
 export function Circle(diameter) {
   this.diameter = diameter;
 }
-
 Circle.prototype.circleArea = function() {
   return (Math.PI/4)*Math.pow(this.diameter,2);
-}
+};
--- src/main.js
+++ src/main.js
@@ -1,4 +1,22 @@
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
-// import $ from 'jquery';
+import $ from 'jquery';
+import './circle.js';
+import { Circle } from './circle.js';
+
+function resetForm(){
+  $("input#diameter").val("");
+}
+
+$(document).ready(function(){
+  $("form#shapes").submit(function(event){
+    event.preventDefault();
+    let inputDiameter = $("input#diameter").val();
+    let circle = new Circle(inputDiameter);
+    let circleAreaResult = circle.circleArea(circle);
+    let displayCircleArea = "The area of a circle with the diameter of " + inputDiameter + ", is " + circleAreaResult + ".";
+    $("p#circleAnswer").text(displayCircleArea);
+    resetForm();
+  });
+});
